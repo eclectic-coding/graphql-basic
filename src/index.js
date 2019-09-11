b8fb869c90c1b462ddb8caa6620605fd3fa9089f
eclecticcoding <chuck@eclecticsaddlebag.com>
2019-09-11 15:32:52 -0400

    Added GQL arrays.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 import { GraphQLServer } from 'graphql-yoga'
 
+// Demo User Data
+const users = [{
+  id: '1',
+  name: 'Andrew',
+  email: 'andrew@example.com',
+  age: 27
+}, {
+  id: '12',
+  name: 'Chuck',
+  email: 'chuck@example.com',
+  age: 57
+}, {
+  id: '13',
+  name: 'Michelle',
+  email: 'michelle@example.com'
+}]
+
+const posts = [{
+  id: '10',
+  title: 'Hello World',
+  body: 'Pius coordinataes ducunt ad turpis.',
+  published: true
+}, {
+  id: '15',
+  title: 'Random Words',
+  body: 'Pius coordinataes ducunt ad turpis.',
+  published: true
+}, {
+  id: '16',
+  title: 'Baseball rules!',
+  body: 'Pius coordinataes ducunt ad turpis.',
+  published: true
+}]
+
 // Type Definitions
 const typeDefs = `
     type Query {
-        greeting(name: String, position: String): String!
-        add(a: Float!, b: Float!): Float!
-        me: User!
-        post: Post!
+       users(query: String): [User!]!
+       posts(query: String): [Post!]!
+       me: User!
+       post: Post!
     }
 
     type User {
@@ -27,15 +61,25 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
   Query: {
-    greeting (parent, args, ctx, info) {
-      if (args.name && args.position) {
-        return `Hello, ${args.name}! You are my favoriate ${args.position}.`
-      } else {
-        return 'Hello!'
+    users (parent, args, ctx, info) {
+      if (!args.query) {
+        return users
       }
+
+      return users.filter((user) => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase())
+      })
     },
-    add (parent, args, ctx, info) {
-      return args.a + args.b
+    posts (parent, args, ctx, info) {
+      if (!args.query) {
+        return posts
+      }
+
+      return posts.filter((post) => {
+        const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+        const isBodyMath = post.body.toLowerCase().includes(args.query.toLowerCase())
+        return isTitleMatch || isBodyMath
+      })
     },
     me () {
       return {
@@ -46,8 +90,8 @@ const resolvers = {
     },
     post () {
       return {
-        id: '092',
-        title: 'GraphQL 101',
+        id: '016',
+        title: 'Baseball Rules!',
         body: '',
         published: false
       }
